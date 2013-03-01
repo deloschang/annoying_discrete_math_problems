09c0cee52c12e87233cf5e95c6de9b07b35a19fd
Delos Chang <lol.i.laugh@gmail.com>
2013-02-28 22:43:18 -0500

    adding variants

--- coinflipper.py
+++ coinflipper.py
@@ -13,16 +13,13 @@ for i in range(2):
         for k in range(2):
             for l in range(2):
                 for m in range(2):
-                    if i + j + k + l + m > 3: 
-                        break;
-                    else: 
-                        convert(i)
-                        convert(j)
-                        convert(k)
-                        convert(l)
-                        convert(m)
-                        print "\n"
-                        counter += 1
+                    convert(i)
+                    convert(j)
+                    convert(k)
+                    convert(l)
+                    convert(m)
+                    print "\n"
+                    counter += 1
 
 print counter
 
