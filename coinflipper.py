a250aaf1c930e1dedb6f0d7777f141f2eded8688
Delos Chang <lol.i.laugh@gmail.com>
2013-02-28 22:35:26 -0500

    done

---
+++ coinflipper.py
@@ -0,0 +1,25 @@
+# to solve annoying discrete mathematics outcome listing problems
+
+def convert(number):
+    if number == 0:
+        print "H",
+    if number == 1:
+        print "T",
+
+i = 0 
+counter = 0 
+for i in range(2):
+    for j in range(2):
+        for k in range(2):
+            for l in range(2):
+                for m in range(2):
+                    convert(i)
+                    convert(j)
+                    convert(k)
+                    convert(l)
+                    convert(m)
+                    print "\n"
+                    counter += 1
+
+print counter
+
